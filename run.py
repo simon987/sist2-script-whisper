1293aa586a974b429c7bb8ae9ede3ffe5bc772a8
write3371 <114498674+write3371@users.noreply.github.com>
2024-05-05 12:46:05 +0800

    Fix https://github.com/simon987/sist2/issues/478

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ from sist2 import Sist2Index, print_progress
 MODELS = {
     "base": "whisper.cpp/models/ggml-base-q4_1.bin",
     "small": "whisper.cpp/models/ggml-small-q4_1.bin",
-    "large": "whisper.cpp/models/ggml-large-q4_1.bin"
+    "large": "whisper.cpp/models/ggml-large-q4_1.bin",
 }
 
 
@@ -19,21 +19,34 @@ def whisper_stt(input_audio: str, num_threads: int, model: str):
     except:
         pass
 
-    subprocess.run([
-        "ffmpeg",
-        "-hide_banner", "-loglevel", "panic",
-        "-i", input_audio,
-        "-ar", "16000",
-        wav_path
-    ])
+    subprocess.run(
+        [
+            "ffmpeg",
+            "-hide_banner",
+            "-loglevel",
+            "panic",
+            "-i",
+            input_audio,
+            "-ar",
+            "16000",
+            wav_path,
+        ]
+    )
 
-    subprocess.run([
-        "whisper.cpp/main",
-        "-t", str(num_threads),
-        "-m", MODELS[model],
-        "-f", wav_path,
-        "--output-txt",
-    ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(
+        [
+            "whisper.cpp/main",
+            "-t",
+            str(num_threads),
+            "-m",
+            MODELS[model],
+            "-f",
+            wav_path,
+            "--output-txt",
+        ],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
 
     with open(wav_path + ".txt", encoding="utf8", errors="ignore") as f:
         text = f.read()
@@ -41,7 +54,13 @@ def whisper_stt(input_audio: str, num_threads: int, model: str):
     return text
 
 
-def main(index_file: str, num_threads: int = 8, color: str = "#51da4c", tag: bool = True, model: str = "base"):
+def main(
+    index_file: str,
+    num_threads: int = 8,
+    color: str = "#51da4c",
+    tag: bool = True,
+    model: str = "base",
+):
     if model not in MODELS:
         raise ValueError(f"Model {model} not found. Possible values: {MODELS.keys()}")
 
@@ -52,8 +71,10 @@ def main(index_file: str, num_threads: int = 8, color: str = "#51da4c", tag: boo
     # Only consider documents that were modified since the last run of this script
     whisper_version = index.get("whisper_version", default=0)
 
-    where = f"((SELECT name FROM mime WHERE id=document.mime) LIKE 'audio/%' " \
-            f"OR (SELECT name FROM mime WHERE id=document.mime) LIKE 'video/%') AND version > {whisper_version}"
+    where = (
+        f"((SELECT name FROM mime WHERE id=document.mime AND  ifnull(content, '') = '') LIKE 'audio/%' "
+        f"OR (SELECT name FROM mime WHERE id=document.mime AND  ifnull(content, '') = '') LIKE 'video/%') AND version > {whisper_version}"
+    )
 
     total = index.document_count(where)
     done = 0
@@ -68,13 +89,17 @@ def main(index_file: str, num_threads: int = 8, color: str = "#51da4c", tag: boo
             if "tags" not in doc.json_data:
                 doc.json_data["tag"] = [tag_value]
             else:
-                doc.json_data["tag"] = list(filter(lambda t: not t.startswith("whisper."), doc.json_data["tag"])) \
-                    .append(tag_value)
+                doc.json_data["tag"] = list(
+                    filter(lambda t: not t.startswith("whisper."), doc.json_data["tag"])
+                ).append(tag_value)
 
-        print(f"Performed STT for {doc.rel_path} ({time() - start:.2f}s)")
+        
         index.update_document(doc)
-
+        print(f"Performed STT for {doc.rel_path} ({time() - start:.2f}s)")
         done += 1
+        if done % 100 == 0:
+            index.commit()
+
         print_progress(done=done, count=total)
 
     index.set("whisper_version", index.versions[-1].id)
